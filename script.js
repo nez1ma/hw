ed20d2ad673618df405ce1161555c863b2169d61
nez1ma <nez703956@gmail.com>
2026-03-12 20:02:22 +0700

    15

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-let num1;
-let num2;
+const numbers = [1, 5, 8, 12, 4, 3, 10, 7, 2];
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 3 && numbers[i] < 10) {
+        console.log(numbers[i]);
+    }
+}
 
-while (true) {
-    let input = prompt("Введіть перше число:");
-    let value = Number(input);
-
-    if (input && input.trim() !== "" && !isNaN(value)) {
-        num1 = value;
+const searchArray = [1, 2, 5, 9, 4, 13, 4, 10];
+for (let i = 0; i < searchArray.length; i++) {
+    if (searchArray[i] === 4) {
+        console.log('Наявний');
         break;
     }
 }
 
-while (true) {
-    let input = prompt("Введіть друге число:");
-    let value = Number(input);
+const mass = [42, 2, 33, 11, 12, 10, 0];
+const average = mass.reduce((total, num) => total + num, 0) / mass.length;
+console.log(average);
 
-    if (input && input.trim() !== "" && !isNaN(value)) {
-        num2 = value;
-        break;
-    }
-}
+const animals = ["parrot", "bull", "bear", "monkey"];
+const totalChars = animals.reduce((sum, word) => sum + word.length, 0);
+console.log(totalChars);
 
-if (num1 % num2 === 0) {
-    alert("Числа кратні!");
-} else {
-    alert(false);
-}
+const mixed = ["parrot", 140, "bull", true, 0, "bear", 47, "monkey"];
+const stringsOnly = mixed.filter(item => typeof item === 'string');
+console.log(stringsOnly);
