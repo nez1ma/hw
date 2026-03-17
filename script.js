c595df13120afd602ff03beb1773b7cde3e9773a
nez1ma <nez703956@gmail.com>
2026-03-17 13:22:16 +0700

    16

--- script.js
+++ script.js
@@ -1,26 +1,56 @@
-const numbers = [1, 5, 8, 12, 4, 3, 10, 7, 2];
-for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > 3 && numbers[i] < 10) {
-        console.log(numbers[i]);
-    }
+function Cat(breed, age, gender, color, name, photoUrl) {
+    this.breed = breed;
+    this.age = age;
+    this.gender = gender;
+    this.color = color;
+    this.name = name;
+    this.photoUrl = photoUrl;
+    this.isSleeping = false;
+
+    this.askForFood = function() {
+        if (this.isSleeping) {
+            alert(this.name + " спить і не може просити їсти");
+        } else {
+            alert(this.name + " просить їсти");
+        }
+    };
+
+    this.layDownToSleep = function() {
+        this.isSleeping = true;
+        renderCats();
+    };
+
+    this.wakeUp = function() {
+        this.isSleeping = false;
+        renderCats();
+    };
 }
 
-const searchArray = [1, 2, 5, 9, 4, 13, 4, 10];
-for (let i = 0; i < searchArray.length; i++) {
-    if (searchArray[i] === 4) {
-        console.log('Наявний');
-        break;
-    }
+const cats = [
+    new Cat("Британська", 3, "Самець", "Сірий", "Барон", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRriVGdSupx--4na8SVQLyMWtuhyhJ-u2uqJA&s.jpg"),
+    new Cat("Сіамська", 2, "Самка", "Кремовий", "Сіма", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp54IGsKgW8Ev-pQdiXMmonDo9GoqAyehpcZBpCVWEbCxwmAMbpctqfhVC6VV08liLJPRXzuyyOj_Sc4qSpJiTAKmH6gA2dD3WWVO5ShE6&s=10.jpg"),
+    new Cat("Мейн-кун", 4, "Самець", "Рудий", "Рижик", "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg")
+];
+
+function renderCats() {
+    const container = document.getElementById('cat-container');
+    container.innerHTML = '';
+
+    cats.forEach((cat, index) => {
+        const card = document.createElement('div');
+        card.className = `cat-card ${cat.isSleeping ? 'sleeping' : ''}`;
+        
+        card.innerHTML = `
+            <img src="${cat.photoUrl}" alt="${cat.name}">
+            <h3>${cat.name}</h3>
+            <p>Порода: ${cat.breed}</p>
+            <p>Статус: ${cat.isSleeping ? 'Спить' : 'Не спить'}</p>
+            <button class="btn-feed" onclick="cats[${index}].askForFood()">Попросити їсти</button>
+            <button class="btn-sleep" onclick="cats[${index}].layDownToSleep()">Лягти спати</button>
+            <button class="btn-wake" onclick="cats[${index}].wakeUp()">Прокинутися</button>
+        `;
+        container.appendChild(card);
+    });
 }
 
-const mass = [42, 2, 33, 11, 12, 10, 0];
-const average = mass.reduce((total, num) => total + num, 0) / mass.length;
-console.log(average);
-
-const animals = ["parrot", "bull", "bear", "monkey"];
-const totalChars = animals.reduce((sum, word) => sum + word.length, 0);
-console.log(totalChars);
-
-const mixed = ["parrot", 140, "bull", true, 0, "bear", 47, "monkey"];
-const stringsOnly = mixed.filter(item => typeof item === 'string');
-console.log(stringsOnly);
+renderCats();
